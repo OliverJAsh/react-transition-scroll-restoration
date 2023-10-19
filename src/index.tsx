558b1131ac98e62f41da7a35453878db546d34b3
Oliver Joseph Ash <oliverjash@gmail.com>
2023-10-19 13:09:56 +0100

    Add minimal router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,40 @@
 import { createRoot } from 'react-dom/client';
+import { Link } from './Link';
+import { Provider } from './ReactHistory';
+import { useURL } from './useURL';
 
-const App = () => 'foo';
+const Nav: React.FC = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href={'/other'}>Other page</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+const Content = () => {
+    const url = useURL();
+    switch (url.pathname) {
+        case '/':
+            return <div>home</div>;
+        case '/other':
+            return <div>other</div>;
+        default:
+            return null;
+    }
+};
+
+const App = () => {
+    return (
+        <Provider>
+            <Nav />
+            <Content />
+        </Provider>
+    );
+};
 
 createRoot(document.getElementById('root')!).render(<App />);
